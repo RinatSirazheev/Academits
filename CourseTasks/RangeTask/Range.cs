6987cf8cc62554add48fa526ff42ad6dd5628d51
RinatSirazheev <sirazheev91@mail.ru>
2020-06-23 14:55:31 +0700

    ReName

--- CourseTasks/RangeTask/Range.cs
+++ CourseTasks/RangeTask/Range.cs
@@ -10,7 +10,7 @@ namespace RangeTask
     {
         static void Main()
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello GitHub!");
         }
     }
 }
