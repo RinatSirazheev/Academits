05cbf244307e727d515c035af0f4c118564a993c
sirazheev91 <sirazheev91@mail.ru>
2020-07-14 18:37:03 +0700

    Задача "Фигуры", сделаны первая и вторая часть задания.

--- CourseTasks/ShapesTask/Shapes.cs
+++ CourseTasks/ShapesTask/Shapes.cs
@@ -1,99 +1,227 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace ShapesTask
 {
-    class Shapes
+   public interface IShape
     {
-        interface IShape
+        string Name { get; }
+        double GetWidth();
+        double GetHeight();
+        double GetArea();
+        double GetPerimetr();
+    }
+
+    public class Square : IShape
+    {
+        public string Name { get; }
+
+        public double SideLength { get; set; }
+
+        public Square(double sideLength)
         {
-            double GetWidth();
-            double GetHeight();
-            double GetArea();
-            double GetPerimetr();
+            SideLength = sideLength;
+            Name = "Квадрат";
         }
 
-        public class Square : IShape
+        public double GetArea()
         {
-            private double sideLength;
-
-            public double SideLength { get; set; }
-
-            public Square(double sideLength)
-            {
-                SideLength = sideLength;
-            }
-
-            public double GetArea()
-            {
-                return Math.Pow(SideLength, 2);
-            }
-
-            public double GetHeight()
-            {
-                return SideLength;
-            }
-
-            public double GetPerimetr()
-            {
-                return 4 * SideLength;
-            }
-
-            public double GetWidth()
-            {
-                return SideLength;
-            }
+            return Math.Pow(SideLength, 2);
         }
 
-        public class Triangle : IShape
+        public double GetHeight()
         {
-            private double x1;
-            private double x2;
-            private double x3;
-            private double y1;
-            private double y2;
-            private double y3;
+            return SideLength;
+        }
 
-            public double X1 { get; set; }
+        public double GetPerimetr()
+        {
+            return 4 * SideLength;
+        }
 
-            public double X2 { get; set; }
+        public double GetWidth()
+        {
+            return SideLength;
+        }
+    }
 
-            public double X3 { get; set; }
+    public class Triangle : IShape
+    {
+        public string Name { get; }
 
-            public double Y1 { get; set; }
+        public double X1 { get; set; }
 
-            public double Y2 { get; set; }
+        public double X2 { get; set; }
 
-            public double Y3 { get; set; }
+        public double X3 { get; set; }
 
-            public Triangle(double x1, double x2, double x3, double y1, double y2, double y3)
+        public double Y1 { get; set; }
+
+        public double Y2 { get; set; }
+
+        public double Y3 { get; set; }
+
+        public Triangle(double x1, double x2, double x3, double y1, double y2, double y3)
+        {
+            Name = "Треугольник";
+            X1 = x1;
+            X2 = x2;
+            X3 = x3;
+            Y1 = y1;
+            Y2 = y2;
+            Y3 = y3;
+        }
+
+        public double GetArea()
+        {
+            return Math.Abs((X2 - X1) * (Y3 - Y1) - (X3 - X1) * (Y2 - Y1)) / 2;
+        }
+
+        public double GetHeight()
+        {
+            return Math.Max(Math.Max(Y1, Y2), Y3) - Math.Min(Math.Min(Y1, Y2), Y3);
+        }
+
+        public double GetPerimetr()
+        {
+            double segmentAB = Math.Sqrt(Math.Pow((X2 - X1), 2) + Math.Pow((Y2 - Y1), 2));
+            double segmentAC = Math.Sqrt(Math.Pow((X3 - X1), 2) + Math.Pow((Y3 - Y1), 2));
+            double segmentBC = Math.Sqrt(Math.Pow((X3 - X2), 2) + Math.Pow((Y3 - Y2), 2));
+
+            return segmentAB + segmentAC + segmentBC;
+        }
+
+        public double GetWidth()
+        {
+            return Math.Max(Math.Max(X1, X2), X3) - Math.Min(Math.Min(X1, X2), X3);
+        }
+    }
+
+    public class Rectangle : IShape
+    {
+        public string Name { get; }
+
+        public double SideLengthA { get; set; }
+
+        public double SideLengthB { get; set; }
+
+        public Rectangle(double sideLengthA, double sideLengthB)
+        {
+            Name = "Прямоугольник";
+            SideLengthA = sideLengthA;
+            SideLengthB = sideLengthB;
+        }
+
+        public double GetArea()
+        {
+            return SideLengthA * SideLengthB;
+        }
+
+        public double GetHeight()
+        {
+            return SideLengthA;
+        }
+
+        public double GetPerimetr()
+        {
+            return SideLengthA * 2 + SideLengthB * 2;
+        }
+
+        public double GetWidth()
+        {
+            return SideLengthB;
+        }
+    }
+
+    public class Circle : IShape
+    {
+        public string Name { get; }
+
+        public double Radius { get; set; }
+
+        public Circle(double radius)
+        {
+            Name = "Круг";
+            Radius = radius;
+        }
+
+        public double GetWidth()
+        {
+            return Radius * 2;
+        }
+
+        public double GetHeight()
+        {
+            return Radius * 2;
+        }
+
+        public double GetArea()
+        {
+            return Math.PI * Math.Pow(Radius, 2);
+        }
+
+        public double GetPerimetr()
+        {
+            return 2 * Math.PI * Radius;
+        }
+    }
+
+    class ShapesAreaComparer : IComparer<IShape>
+    {
+        public int Compare(IShape x, IShape y)
+        {
+            if (x.GetArea() > y.GetArea())
             {
-                X1 = x1;
-                X2 = x2;
-                X3 = x3;
-                Y1 = y1;
-                Y2 = y2;
-                Y3 = y3;
+                return 1;
             }
 
-            public double GetArea()
+            if (x.GetArea() < y.GetArea())
             {
-                return Math.Abs((X2 - X1) * (Y3 - Y1) - (X3 - X1) * (Y2 - Y1)) / 2;
+                return -1;
             }
 
-            public double GetHeight()
+            return 0;
+        }
+    }
+
+    class ShapesPerimetrComparer : IComparer<IShape>
+    {
+        public int Compare(IShape x, IShape y)
+        {
+            if (x.GetPerimetr() > y.GetPerimetr())
             {
-                throw new NotImplementedException();
+                return 1;
             }
 
-            public double GetPerimetr()
+            if (x.GetPerimetr() < y.GetPerimetr())
             {
-                throw new NotImplementedException();
+                return -1;
             }
 
-            public double GetWidth()
-            {
-                throw new NotImplementedException();
-            }
+            return 0;
+        }
+    }
+
+    public static class ShapeMaxAreaFinder
+    {
+        public static void GetShapeMaxArea(this IShape[] shapes)
+        {
+            Array.Sort(shapes, new ShapesAreaComparer());
+
+            Console.WriteLine("{0} является фигурой с самой большой площадью.", shapes[shapes.Length - 1].Name);
+            Console.WriteLine("Ее площадь равна {0}", shapes[shapes.Length - 1].GetArea());
+        }
+    }
+
+    public static class ShapeSecondLargestPerimetrFinder
+    {
+        public static void GetShapeSecondLargestPerimetr(this IShape[] shapes)
+        {
+            Array.Sort(shapes, new ShapesPerimetrComparer());
+
+            Console.WriteLine("{0} является фигурой со вторым по величине периметром.", shapes[shapes.Length - 2].Name);
+            Console.WriteLine("Ее периметр равен {0}", shapes[shapes.Length - 2].GetPerimetr());
         }
     }
 }
---
+++ CourseTasks/ShapesTask/ShapesDemo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ShapesTask
+{
+    public class ShapesDemo
+    {
+        static void Main()
+        {
+            IShape[] shapes = {
+                new Square(5),
+                new Square(8.8),
+                new Rectangle(12, 20.5),
+                new Circle(3.8),
+                new Circle(8),
+                new Triangle(2.4,5,10,2,8,2)
+            };
+                       
+            shapes.GetShapeMaxArea();
+
+            shapes.GetShapeSecondLargestPerimetr();
+        }
+    }
+}
