ca21be2b4f673fa3ab492bf188d2d16439c6a976
sirazheev91 <sirazheev91@mail.ru>
2020-07-18 18:11:36 +0700

    Создан проект Vector

---
+++ CourseTasks/VectorTasks/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VectorTasks
+{
+    class Vector
+    {
+        static void Main()
+        {
+        }
+    }
+}
