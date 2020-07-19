b0ea5f1260ab3b742b5a80694220341aa9e25efa
sirazheev91 <sirazheev91@mail.ru>
2020-07-20 06:32:28 +0700

    Пункты 1-3 из задачи "Вектор"

---
+++ CourseTasks/VectorTasks/Vector.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VectorTasks
+{
+    public class Vector
+    {
+        public double[] NumbersVector { get; set; }
+
+        public Vector(int n)
+        {
+            NumbersVector = new double[n];
+        }
+
+        public Vector(Vector vektor)
+        {
+            NumbersVector = vektor.NumbersVector;
+        }
+
+        public Vector(double[] array)
+        {
+            NumbersVector = array;
+        }
+
+        public Vector(int n, double[] array)
+        {
+            NumbersVector = new double[n];
+
+            if (array.Length < n)
+            {
+                for (int i = 0; i <= array.Length - 1; i++)
+                {
+                    NumbersVector[i] = array[i];
+                }
+
+                for (int i = array.Length; i <= n - 1; i++)
+                {
+                    NumbersVector[i] = 0;
+                }
+            }
+            else
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    NumbersVector[i] = array[i];
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            return string.Join(", ", NumbersVector);
+        }
+
+        public int GetSize()
+        {
+            Vector vector = this;
+
+            return vector.NumbersVector.Length;
+        }
+    }
+}
---
+++ CourseTasks/VectorTasks/VectorDemo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VectorTasks
+{
+    class VectorDemo
+    {
+        static void Main()
+        {
+            double[] p = new double[] { 1, 2, 3, 4, 5 };
+
+            Vector array = new Vector(3,p);
+
+            Console.WriteLine(array);
+
+            Console.WriteLine(array.GetSize());
+        }
+    }
+}
