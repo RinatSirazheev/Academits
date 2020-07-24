331d1171d92d7d57f52f6eebc5ba6e2082a71689
RinatSirazheev <sirazheev91@mail.ru>
2020-07-24 14:34:55 +0700

    Выполнение задачи "Матрица"

--- CourseTasks/MatrixTask/Matrix.cs
+++ CourseTasks/MatrixTask/Matrix.cs
@@ -1,13 +1,37 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using VectorTasks;
 
 namespace MatrixTask
 {
     class Matrix
     {
-       
+        public double[][] MatrixComponents { get; set; }
+
+        public Matrix(int n, int m)
+        {
+            MatrixComponents = new double[n][];
+
+            for (int i = 0; i < n; i++)
+            {
+                Vector v = new Vector(m);
+                MatrixComponents[i] = v.VectorComponents;
+            }
+
+        }
+
+        public override string ToString()
+        {
+            string a = null;
+
+            for (int i = 0; i < MatrixComponents.Length; i++)
+            {
+                a = a + "{" + string.Join(" ,", MatrixComponents[i]) + "}";
+            }
+
+            return "{" + a + "}" ;
+
+
+        }
+
     }
 }
---
+++ CourseTasks/MatrixTask/MatrixDemo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatrixTask
+{
+    class MatrixDemo
+    {
+        static void Main()
+        {
+            Matrix matrix = new Matrix(5, 2);
+
+           Console.WriteLine(" "+ matrix);
+        }
+    }
+}
