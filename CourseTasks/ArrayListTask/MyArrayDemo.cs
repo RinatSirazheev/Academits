92eff813ad15fb0fb69ca52dad40c17eb07cf413
RinatSirazheev <sirazheev91@mail.ru>
2020-08-14 15:59:20 +0700

    Создан проект ArrayListTask

---
+++ CourseTasks/ArrayListTask/MyArrayDemo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayListTask
+{
+    class MyArrayDemo
+    {
+        static void Main()
+        {
+            MyArray<int> list = new MyArray<int>(20);
+
+            Console.WriteLine(list.Capacity);
+
+            list.Capacity = 30;
+
+            Console.WriteLine(list.Capacity);
+
+        }
+    }
+}
