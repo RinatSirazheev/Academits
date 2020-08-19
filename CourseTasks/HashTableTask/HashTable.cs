06bdd0c926a6fc0a77e6442a607947b1b49e9c43
RinatSirazheev <sirazheev91@mail.ru>
2020-08-19 16:02:48 +0700

    Создан проект HashTable

---
+++ CourseTasks/HashTableTask/HashTable.cs
@@ -0,0 +1,75 @@
+﻿using NPOI.SS.Formula.Functions;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTableTask
+{
+    class HashTable<T> : ICollection<T>
+    {
+        private List<T>[] array;
+        private int length;
+
+        public HashTable()
+        {
+            array = new List<T>[100];
+        }
+
+        public HashTable(int capacity)
+        {
+            array = new List<T>[capacity];
+        }
+
+        public int Count { get { return length; } }
+
+        public bool IsReadOnly { get { return true; } }
+
+        public void Add(T item)
+        {
+            int index = Math.Abs(item.GetHashCode() % array.Length);
+
+            if (array[index] == null)
+            {
+                array[index].Add(item);
+            }
+
+            if (array[index] != null)
+            {
+
+            }
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
