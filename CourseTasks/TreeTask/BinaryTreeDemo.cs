405ef92aa96023348acb73a3ec3f3a8ff9e9065d
sirazheev91 <sirazheev91@mail.ru>
2020-09-01 18:07:50 +0700

    Удалены комментарии.

--- CourseTasks/TreeTask/BinaryTreeDemo.cs
+++ CourseTasks/TreeTask/BinaryTreeDemo.cs
@@ -7,26 +7,18 @@ namespace TreeTask
         static void Main()
         {
             var tree = new BinatryTree<double>(8);
-            var treeNumber = new[] {3,10,1,6,4,7,14,13};
-          //  var treeNumber = new[] { 3, 10, 1, 6, 14, 4};
+            var treeNumber = new[] { 3, 10, 1, 6, 4, 7, 14, 13 };
 
             for (int i = 0; i < treeNumber.Length; i++)
             {
-                tree.Add(new TreeNode<double> (treeNumber[i]));
+                tree.Add(new TreeNode<double>(treeNumber[i]));
             }
 
-            //Console.WriteLine(tree.GetParentAt(13).Data);
+            Console.WriteLine(tree.Contains(100));
 
-            //Console.WriteLine( tree.Contains(100));
+            tree.RemoveAt(8);
 
-            ////tree.RemoveAt(8);
-
-            //Console.WriteLine(tree.Contains(8));
-
-            //Console.WriteLine(tree.GetParentAt(14).Data);
-
-
-           // tree.RemoveAt(8);
+            Console.WriteLine(tree.Contains(8));
 
             Console.WriteLine(tree.Count);
             tree.BreadthFirstTraversing();
