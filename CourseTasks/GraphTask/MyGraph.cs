22fc2c66c9fe5adac8cfad5eb1fafccd68c6624b
RinatSirazheev <sirazheev91@mail.ru>
2020-09-04 15:44:38 +0700

    Создан проект MyGraph

---
+++ CourseTasks/GraphTask/MyGraph.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphTask
+{
+    class MyGraph
+    {
+        private bool[] visited;
+
+        public int[,] Graph { get; private set; }
+
+        public MyGraph(int[,] array)
+        {
+            if (array.GetLength(0) != array.GetLength(1))
+            {//TODO дописать исключение!
+                throw new ArgumentException("");
+            }
+
+            Graph = array;
+            visited = new bool[array.GetLength(0)];
+        }
+
+
+
+    }
+}
---
+++ CourseTasks/GraphTask/MyGraphDemo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphTask
+{
+    class MyGraphDemo
+    {
+        static void Main()
+        {
+            var matrix = new[,]
+            {
+                { 0, 1, 0, 0, 0, 0, 0 },
+                { 1, 0, 1, 1, 1, 1, 0 },
+                { 0, 1, 0, 0, 0, 0, 1 },
+                { 0, 1, 0, 0, 0, 0, 0 },
+                { 0, 1, 0, 0, 0, 1, 0 },
+                { 0, 1, 0, 0, 1, 0, 1 },
+                { 0, 0, 1, 0, 0, 1, 0 }
+            };
+
+            var graph = new MyGraph(matrix);
+        }
+    }
+}
