1821db101cc87af8656e55efee60bdc8d50f6355
RinatSirazheev <sirazheev91@mail.ru>
2020-09-08 15:22:05 +0700

    Изменено название проекта.

--- CourseTasks/GraphTask/Properties/AssemblyInfo.cs
+++ CourseTasks/GraphTask/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("GraphTask")]
+[assembly: AssemblyTitle("Graph1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("GraphTask")]
+[assembly: AssemblyProduct("Graph1")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("304fb18a-7540-417b-a311-ea6327fd4398")]
+[assembly: Guid("a3dc3df9-dfc3-49a8-81cd-3f53d4bbfd87")]
 
 // Version information for an assembly consists of the following four values:
 //
