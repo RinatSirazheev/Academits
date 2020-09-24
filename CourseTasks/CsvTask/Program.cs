c5c5cd7ed069b4adef0e7fe22ec38730043c2654
RinatSirazheev <sirazheev91@mail.ru>
2020-09-24 16:59:18 +0700

    Создан проект CsvTask

---
+++ CourseTasks/CsvTask/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsvTask
+{
+    class Program
+    {
+        static void Main()
+        {
+            var inFilePath = "..//..//inFile.txt";
+            var outFilePath = "..//..//outFile.txt";
+            
+            using (var writer = new StreamWriter(outFilePath))
+            {
+                writer.WriteLine("table");
+
+                using (var reader = new StreamReader(inFilePath))
+                {
+                    string currenLine;
+
+                    while ((currenLine = reader.ReadLine()) != null)
+                    {
+                        writer.WriteLine("tr");
+
+                        var a = StringParser.GetTableDetails(currenLine);
+
+
+
+
+                        writer.WriteLine("/tr");
+
+                    }
+                }
+
+                writer.WriteLine("/table");
+            }
+
+        }
+    }
+}
---
+++ CourseTasks/CsvTask/StringParser.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsvTask
+{
+    class StringParser
+    {
+        public static List<string> GetTableDetails(string line)
+        {
+            var list = new List<string>();
+
+            if (line.IndexOf('"') == -1)
+            {
+                var lineSplit = line.Split(new string[] { "," }, StringSplitOptions.RemoveEmptyEntries);
+                
+                return lineSplit.ToList();
+            }
+
+
+
+            return list;
+        }
+
+    }
+}
