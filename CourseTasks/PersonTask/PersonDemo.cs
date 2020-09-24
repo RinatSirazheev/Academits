dc26a9ea0935bc58e99afb7cde7583a88da422ea
RinatSirazheev <sirazheev91@mail.ru>
2020-09-24 10:04:32 +0700

    Исправлены все замечания от 22.09

--- CourseTasks/PersonTask/PersonDemo.cs
+++ CourseTasks/PersonTask/PersonDemo.cs
@@ -9,7 +9,7 @@ namespace PersonTask
     {
         static void Main()
         {
-            var personsList = new List<Person>()
+            var personsList = new List<Person>
             {
                 new Person("Ivan", 30),
                 new Person("Ivan", 14),
@@ -25,14 +25,14 @@ namespace PersonTask
                 .Distinct()
                 .ToList();
 
-            var distinctNamesList = new StringBuilder();
+            var distinctNames = new StringBuilder();
 
-            distinctNamesList.Append("Имена: ")
+            distinctNames.Append("Имена: ")
                              .Append(string.Join(", ", distinctNameslist))
                              .Append(".");
 
             Console.WriteLine("Список содержащий только уникальные имена.");
-            Console.WriteLine(distinctNamesList);
+            Console.WriteLine(distinctNames);
             Console.WriteLine();
 
             var minorPersons = personsList
@@ -46,19 +46,26 @@ namespace PersonTask
             Console.WriteLine("Список людей младше 18 лет: " + string.Join(", ", minorPersons) + ".");
             Console.WriteLine();
 
-            Console.WriteLine("Средний возраст людей младше 18 лет = " + minorPersonsAverageAge);
+            Console.WriteLine("Средний возраст людей младше 18 лет = " + minorPersonsAverageAge + " лет.");
             Console.WriteLine();
 
-            Dictionary<string, double> a = personsList
+            var peoplesAveragesAgesMap = personsList
                 .GroupBy(Person => Person.Name)
-                .ToDictionary(Person => Person.Key, Person => Person.ToList().Select(x => x.Age).Average());
+                .ToDictionary(Person => Person.Key, Person => Person.ToList().Select(p => p.Age).Average());
 
-            Console.WriteLine(a["Ivan"]);
-
-            var personsAged20To40 = personsList.Where(p => p.Age >= 20 && p.Age <= 40).OrderByDescending(p => p.Age).ToList();
-
+            Console.WriteLine("Коллекция в которой ключи - имена, а значения - средний возраст:");
+            foreach (KeyValuePair<string, double> valuePair in peoplesAveragesAgesMap)
+            {
+                Console.WriteLine(valuePair.Key + " - " + valuePair.Value);
+            }
             Console.WriteLine();
 
+            var personsAged20To40 = personsList
+                .Where(Person => Person.Age >= 20 && Person.Age <= 40)
+                .OrderByDescending(Person => Person.Age)
+                .ToList();
+
+            Console.WriteLine("Список людей, возраст которых от 20 до 45 лет:");
             personsAged20To40.ForEach(Console.WriteLine);
         }
     }
